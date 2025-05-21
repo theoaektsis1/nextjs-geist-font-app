787b40c5d3c330c93a3963ed4ba8172114b757b7
theoaektsis1 <104593995+theoaektsis1@users.noreply.github.com>
2025-05-21 17:35:04 +0200

    Add capacitor.config.ts 1747841704985

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ikonmedia.app',
+  appName: 'ikonmediaapp',
+  webDir: 'out'
+};
+
+export default config;
